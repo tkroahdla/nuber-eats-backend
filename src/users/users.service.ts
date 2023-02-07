52777ad1d8cbaafd9e44d47f810a2906b3fcafac
제동진 <43737240+tkroahdla@users.noreply.github.com>
2023-02-07 17:52:50 +0900

    #5.6

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +9,21 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+    try {
+      const exists = await this.users.findOne({ where: { email } });
+      if (exists) {
+        return { ok: false, error: 'There is a user with that email already' };
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
+    } catch (e) {
+      return { ok: false, error: "Could'nt create account" };
+    }
+  }
 }
