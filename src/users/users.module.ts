7ed06673da21cbbd44b89969a4832bdfce5644b9
DJ <tkroahdla@naver.com>
2023-02-13 22:08:44 +0900

    #6.4

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtService } from 'src/jwt/jwt.service';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
